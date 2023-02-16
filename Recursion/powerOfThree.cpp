07f9b756e4967e4533f10c287ab91b641871e0c2
mohit701473 <mohitprajapat7014@gmail.com>
2023-02-17 00:24:23 +0530

    Update powerOfThree.cpp

--- Recursion/powerOfThree.cpp
+++ Recursion/powerOfThree.cpp
@@ -2,7 +2,11 @@
 
 
 // Que : Given an integer n, return true if it is a power of three. Otherwise, return false.
-//       An integer n is a power of three, if there exists an integer x such that n == 3x.
+//       An integer n is a power of three, if there exists an integer x such that n == 3^x.
+
+
+// T(n) = T(n/3) + 1   
+// T.C. = log(n)  base of log is 3
 
 class Solution {
 public: 
