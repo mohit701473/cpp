8cc18787f28e3f8845e28fde98bf47742f902cb1
mohit701473 <mohitprajapat7014@gmail.com>
2023-03-27 22:40:33 +0530

    multiset

---
+++ STL in C++/Sets in  C++/Que_multiset.cpp
@@ -0,0 +1,33 @@
+// Monk and the Magical Candy Bags  (hackerearth.com)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T ;
+	cin>> T ;
+	while(T--){
+		int N, K ;
+		cin>> N >> K ;
+
+		multiset<long long> bag ;
+		for(int i=0 ; i<N ; i++){
+			long long  val ;
+			cin>> val ;
+			bag.insert(val) ;
+		}
+
+		long long max = 0 ;
+		
+		while(K != 0){
+			auto it = (--(bag.end()));
+			//cout<<(*it) ;
+			max += (*it) ;
+			long long new_val = (*it) / 2;
+			bag.erase(it) ;
+			bag.insert(new_val) ;
+			K-- ;
+		}
+
+		cout<<max<<endl;
+	}
+}
