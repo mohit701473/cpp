7887e1c87e4a67260599aa27b449c3063a8c518a
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-03 16:07:03 +0530

    Update Que3_firstNegativeIntegerInSubArray.cpp

--- Stack and Queue/Queue/Question on Queue/Que3_firstNegativeIntegerInSubArray.cpp
+++ Stack and Queue/Queue/Question on Queue/Que3_firstNegativeIntegerInSubArray.cpp
@@ -4,8 +4,7 @@
 // Approch 1
 // T.C. = O((N-K)*K)
 // S.C. = O(K)
-vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int K) {
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
                                                  
         vector<long long> v ;
         
@@ -24,3 +23,55 @@ vector<long long> printFirstNegativeInteger(long long int A[],
         }
           return v ;                                       
  }
+
+
+// Approch 2 using doubley ended queue
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(K)
+
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
+                                                 
+        deque<long long int> dq ;
+        vector<long long> ans ;
+        
+        // process first K element 
+        for(int i=0 ; i<K ; i++){
+            if(A[i]<0){
+                dq.push_back(i) ;
+            }
+        }
+        
+        if(dq.size()>0){
+            ans.push_back(A[dq.front()]) ;
+        }
+        
+        else{
+            ans.push_back(0) ;
+        }
+        
+        // process remaining element 
+        for(int i=K ; i<N ; i++){
+            
+            // Removal 
+            if(!dq.empty() && i - dq.front() >= K){
+                dq.pop_front() ;
+            }
+            
+            // Addition
+            if(A[i]<0){
+                dq.push_back(i) ;
+            }
+            
+            // Store ans 
+            if(dq.size()>0){
+                ans.push_back(A[dq.front()]) ;
+            }
+            
+            else{
+                ans.push_back(0) ;
+            }
+        }
+        
+        return ans ;                                        
+ }
+
