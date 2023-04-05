8d54447181a5e1dd6d7f190a36bf41d1ba069d45
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-05 22:14:51 +0530

    queue

--- Stack and Queue/Queue/Question on Queue/Que8_K_queus_in_array.cpp
+++ Stack and Queue/Queue/Question on Queue/Que8_K_queus_in_array.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 using namespace std ;
 
-class queue{
-    int *arr, *rear, *front , *next;
-    int freespot , n ,k ;
+class kQueue{
     public:
-
-    queue(int k, int k) {
-        this-> n = n ;
+        int n;
+        int k;
+        int *front;
+        int *rear;
+        int *arr;
+        int freespot;
+        int *next;
+    
+    public:
+    kQueue(int n, int k) {
+        this -> n = n ;
         this -> k = k ;
         arr = new int[n] ;
         front = new int[k] ;       // each ith index will represent the front element of ith queue such that arr[front[ith-queue]] is front element of ith queue
@@ -16,12 +22,16 @@ class queue{
         freespot =  0 ;         // this will store next free sopt
 
         for(int i=0 ; i<k ; i++){
-            front[i] = rear[i] = -1 ;
-            next[i] = i+1 ;
-
-            if(i== n-1)
-                next [i] = -1 ;
+            front[i] = -1 ;
+            rear[i] = -1 ;
         }
+            
+        
+
+        for(int i=0 ; i<n ; i++)
+            next[i] = i+1 ;
+            
+        next[n-1] = -1 ;
     }
 
     bool push(int data, int ith_queue){
@@ -42,16 +52,55 @@ class queue{
 
         else {
             // link element of same queue to each other
-            next[rear[ith_queue - 1]] = index
+            next[rear[ith_queue - 1]] = index ;
         }
 
-        // update next
+        // update next bcz now this index is not free
         next[index] = -1 ;
 
         // update rear 
-        rear[ith_queue -1 ] = index ;
+        rear[ith_queue - 1] = index ;
 
         // push element
         arr[index] = data ;
+        return true ;
     }
+
+    int pop(int ith_queue){
+        // check wether queue is empty of not
+        if(front[ith_queue -1] == -1){
+            cout<<"queue is empty"<<endl;
+            return -1;
+        }
+
+        // find the index from which you want to pop element
+        int index = front[ith_queue - 1] ;
+
+        // update the front of ith_queue
+        front[ith_queue -1] = next[index] ;
+
+        // now update next array bcz element is removed from the index so this index is free now 
+        next[index] = freespot ;
+        freespot = index ;
+
+        return arr[index] ;
+
+    }
+};
+
+int main()
+{
+    kQueue q(10,3) ;
+    cout<<"push 10 in queue 1: "<<q.push(10,1)<<endl ;
+    cout<<"push 15 in queue 1: "<<q.push(15,1)<<endl ;
+    cout<<"push 20 in queue 2: "<<q.push(20,2)<<endl ;
+    cout<<"push 25 in queue 1: "<<q.push(25,1)<<endl ;
+
+    cout<<q.pop(1)<<endl;
+    cout<<q.pop(2)<<endl;
+    cout<<q.pop(1)<<endl;
+    cout<<q.pop(1)<<endl;
+
+    return 0 ;
+
 }
