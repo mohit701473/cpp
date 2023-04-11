85500e3a5962ed09cd81365768caf4b182003b36
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-11 21:01:36 +0530

    tree

--- L 62-68 Binary Tree/createBinartTree.cpp
+++ L 62-68 Binary Tree/createBinartTree.cpp
@@ -65,105 +65,45 @@ void levelOrderTraversal(Node* root){
     }
 }
 
-void recursive_inorder(Node* root){ // left root right
-    if(root == NULL)
-        return ;
 
-    recursive_inorder(root -> left) ;
-    cout<<root -> data<<" ";
-    recursive_inorder(root -> right) ;
-}
+void buildTreeFromLevelOrderTraversal(Node* &root){
+    queue<Node*> q ;
+    cout<<"enter data for the root Node"<<endl;
+    int data ;
+    cin>> data ;
+    root = new Node(data) ;
+    q.push(root) ;
 
-void itrative_inorder(Node* root){
-    if(root == NULL){
-        cout<<"height of tree is zeor"<<endl;
-        return ;
-    }
+    while(!q.empty()) {
+        Node* temp = q.front() ;
+        q.pop() ;
 
-    stack< Node* > s ;
-    s.push(root) ;
-    bool flage = false ; 
+        cout<<"enter data for left of "<<temp -> data<<endl;
+        int leftData ;
+        cin>> leftData ;
 
-    while(!s.empty()){
-
-        if(root == NULL){
-
-            root = s.top() ;
-            s.pop() ;
-            cout<<root -> data <<" " ;
-            root = root -> right ;
-
-            if(root != NULL){
-                s.push(root) ;
-               flage = false ;
-            }
+        if(leftData != -1){
+            temp -> left = new Node(leftData) ;
+            q.push(temp -> left) ;
         }
 
-        else{
-            if(flage){
-                s.push(root) ;
-            }
-            root = root -> left ;
-            flage = true ;
+        cout<<"enter data for right of "<<temp -> data<<endl;
+        int rightData ;
+        cin>> rightData ;
 
+        if(rightData != -1){
+            temp -> right = new Node(rightData) ;
+            q.push(temp -> right) ;
         }
     }
 }
-
-
-
-void recursive_preorder(Node* root){ // root left right
-    if(root == NULL)
-        return ;
-
-    cout<<root -> data<<" ";
-    recursive_preorder(root -> left) ;
-    recursive_preorder(root -> right) ;
-}
-
-void itrative_preorder(Node* root){
-   if(root == NULL){
-        cout<<"height of tree is zeor"<<endl;
-        return ;
-    } 
-
-    
-
-}
-
-void recursive_postorder(Node* root){ // left right root
-    if(root == NULL)
-        return ;
-
-    recursive_postorder(root -> left) ;
-    recursive_postorder(root -> right) ;
-    cout<<root -> data<<" ";
-}
-
-
 
 
 int main() 
 {
     Node* root = NULL ; 
-    root = buildTree(root) ;  // 1 3 7 -1 -1 11 -1 -1 5  17 -1 -1 -1  or 
+    //root = buildTree(root) ;  // 1 3 7 -1 -1 11 -1 -1 5  17 -1 -1 -1  or 
+    buildTreeFromLevelOrderTraversal(root) ;
     levelOrderTraversal(root) ;
-
-    cout<<"recursive_inorder traversal"<<endl;
-    recursive_inorder(root) ;
-    cout <<endl;
-
-    // cout<<"recursive_preorder traversal"<<endl;
-    // recursive_preorder(root) ;
-    // cout <<endl;
-
-    // cout<<"recursive_postorder traversal"<<endl;
-    // recursive_postorder(root) ;
-    // cout <<endl;
-
-    cout<<"itrative_inorder traversal"<<endl;
-    itrative_inorder(root) ;
-    cout<<endl;
-
 
 }
