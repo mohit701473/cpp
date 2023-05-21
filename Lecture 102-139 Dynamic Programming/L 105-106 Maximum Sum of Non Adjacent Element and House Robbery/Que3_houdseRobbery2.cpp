a0b4cd2457de70f5883c8763f43ce04f2be02143
mohit701473 <mohitprajapat7014@gmail.com>
2023-05-21 17:32:30 +0530

    Update Que3_houdseRobbery2.cpp

--- Lecture 102-139 Dynamic Programming/L 105-106 Maximum Sum of Non Adjacent Element and House Robbery/Que3_houdseRobbery2.cpp
+++ Lecture 102-139 Dynamic Programming/L 105-106 Maximum Sum of Non Adjacent Element and House Robbery/Que3_houdseRobbery2.cpp
@@ -4,7 +4,8 @@
 
 // Approch -> 1 recursive approch
 // T.C. => exponential
-// S.C. => Exponentiallong long int solveRecursive(vector<int>& valueInHouse, int N, int i){
+// S.C. => Exponential
+long long int solveRecursive(vector<int>& valueInHouse, int N, int i){
     if( i>=N )
         return 0 ;
 
@@ -24,3 +25,48 @@ long long int houseRobber(vector<int>& valueInHouse)
 
     return max(solveRecursive(valueInHouse, n-1, 0), solveRecursive(valueInHouse, n, 1)) ;
 }
+
+
+
+
+// Approch -> 2 Top-Down Approch(recursion + memoization)
+// T.C. => O(n)
+// S.C. => O(n)
+long long int solveMemoization(vector<int> &valueInHouse, int N, int i, vector<long long int> &dp){
+
+    if( i >= N)
+        return 0 ;
+
+    if(dp[i] != -1) 
+        return dp[i] ;
+
+    long long int incl = valueInHouse[i] + solveMemoization(valueInHouse, N, i+2, dp) ;
+    long long int excl = solveMemoization(valueInHouse, N, i+1, dp) + 0 ;
+
+    dp[i] =  max(incl, excl) ;
+    return dp[i] ;
+}
+
+
+long long int houseRobber(vector<int>& valueInHouse)
+{   
+    int n = valueInHouse.size() ;
+    vector<long long int> dp(n, -1) ;
+
+    if(n==1)
+        return valueInHouse[0] ;
+
+    long long int ans1 = solveMemoization(valueInHouse, n-1, 0, dp) ;  
+    //cout<<"ans1 -> "<<ans1 <<endl;
+
+    for(auto &i: dp)
+        i = -1 ;
+
+    long long int ans2 = solveMemoization(valueInHouse, n, 1, dp) ;
+    //cout<<"ans2 -> "<<ans2<<endl;
+
+
+    return max(ans1, ans2) ;
+}
+
+
