ece0c0a639419a9ea130c9bfb481b65e4f8a6178
mohit701473 <mohitprajapat7014@gmail.com>
2023-06-05 15:18:41 +0530

    Create Que4_reorderList.cpp

---
+++ Interviewbit/Linked List Questions/List Trick/Que4_reorderList.cpp
@@ -0,0 +1,76 @@
+https://www.interviewbit.com/problems/reorder-list/
+
+
+//------------------------------------------------------------   Solution    ----------------------------------------------------------------------
+
+
+// Approch - 1
+// T.C. = O(N)
+// S.C. = O(1)
+
+ListNode* getMiddleOfList(ListNode* head){
+    ListNode* slow = head ;
+    ListNode* fast = head -> next ;
+    
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next ;
+        fast = fast -> next -> next ;
+    }
+    
+    return slow ;
+} 
+
+ListNode* reverseLinkedList(ListNode* head){
+    if(head == NULL || head -> next == NULL)
+        return head ;
+        
+    ListNode* curr = head ;
+    ListNode* prev = NULL ;
+    ListNode* forward = NULL ;
+    
+    while(curr != NULL){
+        forward = curr -> next ;
+        curr -> next = prev ;
+        prev = curr ;
+        curr = forward ;
+    }
+    
+    return prev ;
+}
+
+ListNode* mergedList(ListNode* l1, ListNode* l2){
+    ListNode* head = l1 ;
+    
+    ListNode* forward1 = NULL ;
+    ListNode* forward2 = NULL ;
+    
+    while(l1 != NULL && l2 != NULL){
+        forward1 = l1 -> next ;
+        forward2 = l2 -> next ;
+        
+        l1 -> next = l2 ;
+        l2 -> next = forward1 ;
+        
+        l1 = forward1 ;
+        l2 = forward2 ;
+    }
+    
+    return head ;
+}
+
+ListNode* Solution::reorderList(ListNode* A) {
+    if(A == NULL || A -> next == NULL)
+        return A ;
+        
+    // step -> 1: find middle of Linked List
+    ListNode* mid = getMiddleOfList(A) ;
+    
+    // step -> 2 : separate list into two list 
+    ListNode* l1 = A ;
+    ListNode* l2 = mid -> next ;
+    mid -> next = NULL ;
+    
+    l2 = reverseLinkedList(l2) ;
+    
+    // step -> 3: merge these two list
+    return mergedList
