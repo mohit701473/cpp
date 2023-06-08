4585f6e5945bed1930cfef5646fe6516ce5ab364
mohit701473 <mohitprajapat7014@gmail.com>
2023-06-08 14:01:08 +0530

    Create Que3_insertionSortInLL.cpp

---
+++ Interviewbit/Linked List Questions/List Sort/Que3_insertionSortInLL.cpp
@@ -0,0 +1,46 @@
+https://www.interviewbit.com/problems/insertion-sort-list/
+
+
+//------------------------------------------------------------   Solution    ----------------------------------------------------------------------
+
+
+// Approch - 1
+// T.C. = O(N^2)
+// S.C. = O(1)
+ListNode* Solution::insertionSortList(ListNode* A) {
+    if(A == NULL || A -> next == NULL)
+        return A ;
+        
+    ListNode* curr = A ;
+    
+    while(curr != NULL){
+        bool flag = true ;
+        
+        ListNode* temp = curr -> next ;
+        ListNode* tempCurr = A ;
+        ListNode* tempPrev = NULL ;
+        while(tempCurr != temp && temp != NULL){
+            if(tempCurr -> val > temp -> val){
+                if(tempPrev == NULL){
+                    A = temp ;
+                    curr -> next = temp -> next ;
+                    temp -> next = tempCurr ;
+                }else{
+                    tempPrev -> next = temp ;
+                    curr -> next = temp -> next ;
+                    temp -> next = tempCurr ;
+                }
+                flag = false ;
+                break ;
+            }
+            
+            tempPrev = tempCurr ;
+            tempCurr = tempCurr -> next ;
+        }
+        
+        if(flag)
+            curr = curr -> next ;
+    }
+    
+    return A ;
+}
