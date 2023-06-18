c366be111e214154838a0451d20a95a015ae7f83
mohit701473 <mohitprajapat7014@gmail.com>
2023-06-18 20:29:50 +0530

    Update Que1_longestSubstringWithoutRepeating.cpp

--- DSA Questions/Topic - 7 Sliding Window and Two Pointer Questions/Que1_longestSubstringWithoutRepeating.cpp
+++ DSA Questions/Topic - 7 Sliding Window and Two Pointer Questions/Que1_longestSubstringWithoutRepeating.cpp
@@ -42,7 +42,16 @@ public:
 // T.C. = O(N)
 // S.C. = O(N) 
 /*
-Algo -> 
+Algo -> We take two pointer i & j initially i=j=0 and we also take a map in which key = string character, value = pair(index, bool)
+
+        ch = s[j] (jth character of string)
+        We traverse the entire string and we check the following condition
+        1st condition -> if ch is not visited then we store the index of it & mark visited = true
+        2nd condition -> if ch is visited then we update our window by cahnging the position of i such that if i < index of ch + 1
+                        (i.e i < visited[ch].first + 1) then we upadate i bcz when we don't apply this condition then in the case of "abba" ans is 
+                        wrong bcz 
+        and every time we upadte the length
+        
 */
 class Solution {
 public:
@@ -52,16 +61,24 @@ public:
 
         while(j<n){
             char ch = s[j] ;
+
+            // condition -> 1st
             if(!visited[ch].second){
                 visited[ch].second = true ;
                 visited[ch].first = j ;
                 j++ ;
-            }else{
+            }
+            
+            // // condition -> 2nd
+            else{
+
+                // upadte the i
                 if(visited[ch].first + 1 > i)
                     i = visited[ch].first + 1 ;
                 visited.erase(ch) ;
             }
 
+            // update the length
             len = max(len, j-i) ;
         }
 
