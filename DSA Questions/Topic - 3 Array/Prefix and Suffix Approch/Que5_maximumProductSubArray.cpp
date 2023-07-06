6b5e21b667f0c935287bf9b5f43d52292c084d95
mohit701473 <mohitprajapat7014@gmail.com>
2023-07-07 00:14:27 +0530

    Update Que5_maximumProductSubArray.cpp

--- DSA Questions/Topic - 3 Array/Prefix and Suffix Approch/Que5_maximumProductSubArray.cpp
+++ DSA Questions/Topic - 3 Array/Prefix and Suffix Approch/Que5_maximumProductSubArray.cpp
@@ -12,34 +12,19 @@ https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/0
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size() ;
-        int prefix = 1, suffix = 1, ans = INT_MIN ;
-        
-        for(int i=0 ; i<n ; i++){
-            if(prefix == 0) prefix = 1 ;
-            if(suffix == 0) suffix = 1 ;
 
-            prefix *= nums[i] ;
-            suffix *= nums[n-i-1] ;
+        int ans = INT_MIN ;
 
-            ans = max(ans, max(prefix, suffix)) ;
+        for(int i=0 ; i<nums.size() ; i++){
+            int product = 1 ;
+            for(int j=i ; j<nums.size() ; j++){
+                product *= nums[j] ;
+
+                ans = max(ans, product) ;
+            }
         }
 
         return ans ;
-
-
-        // int ans = INT_MIN ;
-
-        // for(int i=0 ; i<nums.size() ; i++){
-        //     int product = 1 ;
-        //     for(int j=i ; j<nums.size() ; j++){
-        //         product *= nums[j] ;
-
-        //         ans = max(ans, product) ;
-        //     }
-        // }
-
-        // return ans ;
     }
 };
 
